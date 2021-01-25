e01b501f670d503294e2f42a576d0c8e7dfef628
Naomi Tague <ctague@bren.ucsb.edu>
2021-01-25 07:51:28 -0800

    Update test_spring_summary.R
    add some comments

--- Tests/test_spring_summary.R
+++ Tests/test_spring_summary.R
@@ -1,4 +1,10 @@
 test_that("spring_summary_works" ,
+ # Generate Climate Data where there is a 2 years each with 2 months (so 4 months in total), and each month just has one day - and we can pick one to call it spring
+  # Simple way to test because its 'easy' to see what the function should return if working correctly
+
+         
+          
+          
 {clim_data = as.data.frame(cbind(month=c(1:4), day=rep(1, times=4), year=c(2000,2000,2001,2001), 
                                  precip=rep(0, times=4), tavg=c(2,2,2,4)))
 
